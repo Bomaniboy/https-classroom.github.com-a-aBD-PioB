0b359995f83d62effb8a20ff2b1f652c822addb4
Bomaniboy <odero.ajamu@yahoo.com>
2019-08-29 20:49:04 -0400

    Finish la 1

---
+++ my_name_is/my_name_is.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(){
+
+  //declarations
+  string name = "Odero Ajamu";
+
+
+  //output
+  cout<<"My Name is: " << name <<endl;
+
+
+
+  return 0;
+}
